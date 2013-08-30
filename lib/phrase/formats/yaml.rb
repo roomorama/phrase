5053c737c9cd272b2f875b4746f93c9db4897cdb
Donald Piret <donald@donaldpiret.com>
2013-08-30 18:39:16 +0800

    Update yaml.rb

--- lib/phrase/formats/yaml.rb
+++ lib/phrase/formats/yaml.rb
@@ -4,7 +4,7 @@ module Phrase
   module Formats
     class Yaml < Phrase::Formats::Base
       def self.filename_for_locale(locale)
-        "phrase.#{locale.name}.yml"
+        "#{locale.name}.yml"
       end
       
       def self.locale_aware?
